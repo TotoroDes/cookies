ff292ceade17d3e9578cedd48aaebf767f4c2d04
TotoroDes <totoro154@gmail.com>
2018-07-08 18:29:09 +0300

    cookies

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+const DIST_PATH = path.resolve(__dirname, './dist');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: DIST_PATH,
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules|dist/,
+            use: 'babel-loader',
+        },
+         {
+             test: /\.css$/,
+             exclude: /node_modules|dist/,
+             use: [
+                    'style-loader',
+                    'css-loader'
+              ]
+          }
+        ],
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: DIST_PATH,
+        port: 7788,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './templates/index.ejs',
+            title: 'Cookies',
+        }),
+    ],
+};
